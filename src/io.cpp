47dfe813f9567cf4d18ed6d85012d9f402e6cec0
Andrea Orrù <andrew.iwakura@gmail.com>
2014-01-25 12:41:53 +0100

    Accelerated graphics.

--- src/io.cpp
+++ src/io.cpp
@@ -4,33 +4,40 @@
 
 namespace IO {
 
+const unsigned width  = 256;
+const unsigned height = 240;
+
 SDL_Window* window;
 SDL_Renderer* renderer;
 SDL_Texture* texture;
-u32 pixels[256 * 240];
+u32 pixels[width * height];
 
 void init()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
 
-    SDL_CreateWindowAndRenderer(256, 240, 0, &window, &renderer);
-    texture = SDL_CreateTexture(renderer,
-                                SDL_PIXELFORMAT_ARGB8888,
-                                SDL_TEXTUREACCESS_STREAMING,
-                                256, 240);
+    window   = SDL_CreateWindow  ("LaiNES",
+                                  SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                  width, height, 0);
+
+    renderer = SDL_CreateRenderer(window, -1,
+                                  SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    texture  = SDL_CreateTexture (renderer,
+                                  SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING,
+                                  width, height);
 
     signal(SIGINT, SIG_DFL);
 }
 
 void draw_pixel(unsigned x, unsigned y, u32 rgb)
 {
-    pixels[y*256 + x] = rgb;
+    pixels[y*width + x] = rgb;
 }
 
 void flush_screen()
 {
-    SDL_UpdateTexture(texture, NULL, pixels, 256 * sizeof (u32));
+    SDL_UpdateTexture(texture, NULL, pixels, width * sizeof (u32));
     SDL_RenderClear(renderer);
     SDL_RenderCopy(renderer, texture, NULL, NULL);
     SDL_RenderPresent(renderer);
