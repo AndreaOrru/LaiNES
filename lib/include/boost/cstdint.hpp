edd8557c3b761454fcc568752351126b8e95eec2
Jeff Katz <git@kraln.com>
2026-02-28 15:26:07 +0100

    Monkeypatch blargg_common.h
    A few minor patches to the blargg library so it compiles cleanly
    under windows *and* macos/linux:
    
    - Add #include <new>
    - Replace boost/cstdint.hpp with standard <cstdint>
    - Use std::nothrow directly instead of STD::nothrow macro
    
    Since the target is C++14, this was just causing problems.

--- lib/include/boost/cstdint.hpp
+++ lib/include/boost/cstdint.hpp
@@ -4,39 +4,18 @@
 #ifndef BOOST_CSTDINT_HPP
 #define BOOST_CSTDINT_HPP
 
-#if BLARGG_USE_NAMESPACE
-	#include <climits>
-#else
-	#include <limits.h>
-#endif
+#include <cstdint>
+#include <climits>
 
 BLARGG_BEGIN_NAMESPACE( boost )
 
-#if UCHAR_MAX != 0xFF || SCHAR_MAX != 0x7F
-#   error "No suitable 8-bit type available"
-#endif
-
-typedef unsigned char   uint8_t;
-typedef signed char     int8_t;
-
-#if USHRT_MAX != 0xFFFF
-#   error "No suitable 16-bit type available"
-#endif
-
-typedef short           int16_t;
-typedef unsigned short  uint16_t;
-
-#if ULONG_MAX == 0xFFFFFFFF
-	typedef long            int32_t;
-	typedef unsigned long   uint32_t;
-#elif UINT_MAX == 0xFFFFFFFF
-	typedef int             int32_t;
-	typedef unsigned int    uint32_t;
-#else
-#   error "No suitable 32-bit type available"
-#endif
+using ::uint8_t;
+using ::int8_t;
+using ::uint16_t;
+using ::int16_t;
+using ::uint32_t;
+using ::int32_t;
 
 BLARGG_END_NAMESPACE
 
 #endif
-
